5980d20781830c24c8659fbe69404e8d8b862e80
Codey Pinero <codey.pinero@gmail.com>
2019-02-15 07:32:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(object, {[key]: value})
+  return Object.assign({[key]: value}, object)
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
