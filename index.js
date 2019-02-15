50e88fa3f00cbf994c1840a4c5551c4d23cf6bfe
Codey Pinero <codey.pinero@gmail.com>
2019-02-15 07:31:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  object = Object.assign({}, object)
+  delete object.key
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return object.key
+}
